0630b38f966b688cbe1fe3cb567b038373386903
vlutin <lowrider>
2017-08-02 14:44:11 -0600

    jquery and fetch exercise

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,70 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        $.ajax(
+            {
+                url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+                headers: {
+                    Authorization: 'Client-ID 2b34233aac3d6ffef3c9651e4f518f0fbc58c11d122ea01b3ef45989986ff744'
+                }
+            }
+        )
+        .done(addImage)
+        .fail(function(err) {
+            requestError(err, 'image');
+        });
+
+        $.ajax(
+            {
+                url:`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=630666f4439747309f4a9b2e1f265b84`
+            }
+        )
+        .done(addArticle)
+        .fail(function(err) {
+            requestError(err, 'articles');
+        });
+;
+
     });
+
+
+    function addImage(images) {
+      let htmlContent = '';
+      const firstImage = images.results[0];
+
+      if(images && images.results && images.results[0]) {
+          htmlContent = `<figure>
+            <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+          </figure>`;
+      } else {
+        htmlContent = '<div class="error-no-image">No Images available</div>';
+      }
+
+      responseContainer.insertAdjacentHTML("afterbegin", htmlContent);
+    }
+
+    function addArticle(articles) {
+      let htmlContent = '';
+
+      if(articles.response && articles.response.docs && articles.response.docs.length > 1) {
+      htmlContent = '<ul>'+ articles.response.docs.map(article => `<li class="article">
+            <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+            <p>${article.snippet}</p>
+         </li>`
+         ).join('')+ '</ul>'
+        
+      } else {
+        htmlContent = '<div class="error-no-article">No Articles available</div>';
+      }
+
+      responseContainer.insertAdjacentHTML("afterbegin", htmlContent);
+    }
+
+    function requestError(e, part) {
+      console.log(e);
+      responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning error-no-${part}">something wrong!</p>`)
+    }
+
 })();
--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -8,5 +8,61 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+            headers: {
+                Authorization: 'Client-ID 2b34233aac3d6ffef3c9651e4f518f0fbc58c11d122ea01b3ef45989986ff744'
+            }
+        })
+        .then(response => response.json())
+        .then(addImage) // response.json returns a new promise from Responde.body parsing to json
+        .catch(e => requestError(e, 'image'))
+        ;
+
+        fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=630666f4439747309f4a9b2e1f265b84`)
+        .then(response => response.json())
+        .then(addArticle)
+        .catch(e => requestError(e, 'articles'))
+        ;
+
     });
+
+    function addImage(images) {
+      let htmlContent = '';
+      const firstImage = images.results[0];
+
+      if(images && images.results && images.results[0]) {
+          htmlContent = `<figure>
+            <img src="${firstImage.urls.small}" alt="${searchedForText}">
+            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+          </figure>`;
+      } else {
+        htmlContent = '<div class="error-no-image">No Images available</div>';
+      }
+
+      responseContainer.insertAdjacentHTML("afterbegin", htmlContent);
+    }
+
+    function addArticle(articles) {
+      let htmlContent = '';
+
+      if(articles.response && articles.response.docs && articles.response.docs.length > 1) {
+      htmlContent = '<ul>'+ articles.response.docs.map(article => `<li class="article">
+            <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+            <p>${article.snippet}</p>
+         </li>`
+         ).join('')+ '</ul>'
+        
+      } else {
+        htmlContent = '<div class="error-no-article">No Articles available</div>';
+      }
+
+      responseContainer.insertAdjacentHTML("afterbegin", htmlContent);
+    }
+
+    function requestError(e, part) {
+      console.log(e);
+      responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning error-no-${part}">something wrong!</p>`)
+    }
+
 })();
